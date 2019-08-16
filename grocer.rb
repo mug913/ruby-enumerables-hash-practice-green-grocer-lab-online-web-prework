070534517fc5458ccae0b31833fc5240a7304627
Brad Alexander <brad.alexander913@gmail.com>
2019-08-16 02:01:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+  full_cart = Hash.new
+    keycount = "blank"
+  cart.map() do |item|
+  item.reduce(full_cart) do |memo, (key,value)|
+  
+    num = 1 
+    if (key == keycount)
+      full_cart[key][:count] = num + 1 
+      num += 1
+    else
+      num = 1
+      full_cart[key] = value
+      full_cart[key][:count] = num
+      keycount = key
+      
+    end
+    end
+    
+  end
+  puts full_cart
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +32,10 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+cart =     [
+      {"AVOCADO" => {:price => 3.00, :clearance => true }},
+      {"AVOCADO" => {:price => 3.00, :clearance => true }},
+      {"KALE"    => {:price => 3.00, :clearance => false}}
+    ]
+consolidate_cart(cart)
